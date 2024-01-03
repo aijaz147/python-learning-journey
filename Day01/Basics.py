28a581ad149c488ed585a4f9f5ddfbfdd9db86f9
Aijaz Ahmed <ahmedaijaz147@gmail.com>
2024-01-03 06:55:27 +0500

    Comments, Escape Sequences, Print Statement

---
+++ Day01/Basics.py
@@ -0,0 +1,21 @@
+print("Hello World")
+# this is my first program in Python # code will not run
+print("Hello\ngood morning") # escape character sequence
+
+'''
+p = 7
+if (p > 5):
+    print("p is greater than 5.")
+else:
+    print("p is not greater than 5.")
+'''
+# this was multi line comment 
+
+print("hello,\"Good Morning.\"") # hello,"Good Morning."
+print("Hello", 100, True, sep = "-") # more value in print
+
+
+
+
+
+
