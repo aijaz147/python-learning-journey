6c870f1bcadc82cf687efad3c9207145a1dad69b
Aijaz Ahmed <ahmedaijaz147@gmail.com>
2024-01-03 06:55:58 +0500

    Variables and Data Types

---
+++ Day01/variables_dataTypes.py
@@ -0,0 +1,21 @@
+# Variables are used to store data in the momory
+# DataTypes are types of variables
+
+userName = "Tonny" # variable and data type is string
+print(type (userName)) # we can see the data type
+print(userName)
+
+age = 24  # variable and data type is int
+print(type (age))
+print(age)
+
+isLoggedIn = False # variable and data type is boolean 
+print(type (isLoggedIn))
+print(isLoggedIn)
+
+print("the type of age is", type (age)) # the type of age is <class 'int'>
+
+# in python all is object 
+
+
+
