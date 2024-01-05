3959d966d2eaff13a1b15277aab905c71dac62a2
Aijaz Ahmed <ahmedaijaz147@gmail.com>
2024-01-05 07:02:24 +0500

    String Methods, String Slicing

---
+++ Day03/stringMethods.py
@@ -0,0 +1,31 @@
+userName = "Kevin Martin"
+print(len(userName))
+# strings are  immutable
+print(userName.upper()) # Uppercase
+print(userName.lower()) # Lowercase
+
+city = "Tokyo!!!"
+print(city.rsplit("!"))
+
+print(userName.replace("Kevin Martin", "John"))
+print(userName.split())
+
+print(userName.capitalize())
+
+name2 = "           Tonny"
+print(len(name2))
+print(name2.center(16))
+
+str1 = "Pyhton is a programming language"
+print(str1.endswith("ge"))
+
+print(str1.find("language"))
+
+print(str1.index("is"))
+
+name4 = "WafaPathan09"
+print(name4.isalnum())
+
+print(name4.isalpha())
+print(name4.islower())
+print(name4.isupper())
---
+++ Day03/stringsSlicing.py
@@ -0,0 +1,13 @@
+name = "Tonny"
+
+print(len(name)) # length of "name" is 5
+print(name[:5]) # slicing from start
+print(name[2:4]) #slicing in between
+
+
+
+
+
+
+
+
